7b1b6c6d2554a9ce79efe3f21fc3d5d6fa7f3cb5
ayazizi <aymenazizi61@gmail.com>
2025-02-19 12:42:23 +0100

    fixing the check

--- functions/GeneratingTheAsciiArt.go
+++ functions/GeneratingTheAsciiArt.go
@@ -6,6 +6,10 @@ import (
 )
 
 func GeneratingTheAsciiArt(w http.ResponseWriter, banner string, userText string) (string, bool) {
+	if !isClean(userText) {
+		RenderPageNotFound(w, http.StatusBadRequest)
+		return "", true
+	}
 	for i := 0; i < len(userText); i++ {
 		if !(userText[i] >= 32 && userText[i] <= 126) && !(strings.Contains(userText, "\r\n")) {
 			RenderPageNotFound(w, http.StatusBadRequest)
@@ -24,4 +28,16 @@ func GeneratingTheAsciiArt(w http.ResponseWriter, banner string, userText string
 		return "", true
 	}
 	return asciiArt, false
+}
+
+func isClean(str string) bool {
+	for i := 0; i < len(str); i++ {
+		if (str[i] == '\r' && str[i+1] == '\n') || str[i] == '\n' {
+			continue
+		}
+		if str[i] < 32 || str[i] > 126 {
+			return false
+		}
+	}
+	return true
 }
