c53f0eec961a86369763479d397f75247cafced0
AymenOski <aymenazizi610@gmail.com>
2025-03-15 12:21:39 +0000

    Adding last check

--- functions/StyleHandler.go
+++ functions/StyleHandler.go
@@ -6,6 +6,10 @@ import (
 )
 
 func StyleHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		RenderError(w,http.StatusMethodNotAllowed)
+		return
+	}
 	rr, err := os.Stat(r.URL.Path[1:])
 	if err != nil || rr.IsDir() {
 		RenderError(w, http.StatusNotFound)
